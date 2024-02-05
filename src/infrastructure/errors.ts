0fc538adebdfa221c44a3000ae992f675a6ca901
Azat Davliatshin <ardavlyatshin@gmail.com>
2024-02-05 12:32:18 +0300

    feat(EPMLSTRCHC-2) Project Setup - add provider for checker; - complete basic workflow;

--- src/infrastructure/errors.ts
+++ src/infrastructure/errors.ts
@@ -1,7 +1,9 @@
 export enum ERRORS {
   WRONG_FILE_FORMAT,
+  UNKNOWN_ERROR,
 }
 
 export const ERROR_MESSAGES: Record<ERRORS, string> = {
   [ERRORS.WRONG_FILE_FORMAT]: "File format should be .mol or .ket",
+  [ERRORS.UNKNOWN_ERROR]: "Error is occured: ",
 } as const;
--- src/processors/InspectoProcessor.ts
+++ src/processors/InspectoProcessor.ts
@@ -5,6 +5,7 @@ import {
   IIndigoProvider,
   ERRORS,
   ERROR_MESSAGES,
+  IMoleculeCheckerProvider,
 } from "@infrastructure";
 import { inject, injectable } from "inversify";
 
@@ -14,24 +15,34 @@ export class InspectoProcessor implements IInspectoProcessor {
     @inject(TOKENS.FILE_PROVIDER) private readonly _fileProvide: IFileProvider,
     @inject(TOKENS.INDIGO_PROVIDER)
     private readonly _indigoProvider: IIndigoProvider,
+    @inject(TOKENS.MOLECULE_CHECKER_PROVIDER)
+    private readonly _moleculeChecker: IMoleculeCheckerProvider,
   ) {}
 
-  public async checkMoleculeFromFileForRules(path: string): Promise<void> {
+  public async checkMoleculeFromFileForRules(path: string): Promise<string[]> {
     const moleculeFileContentResponse =
       await this._fileProvide.getFileContent(path);
     if (moleculeFileContentResponse === ERRORS.WRONG_FILE_FORMAT) {
       throw new Error(ERROR_MESSAGES[moleculeFileContentResponse]);
     } else {
-      try {
-        const ketMolecule = await this._indigoProvider.convertToKetFormat(
-          moleculeFileContentResponse,
-        );
-        console.log(ketMolecule);
-      } catch (error) {
-        // throw a proper exception
-      }
+      return await this._handleMoleculeChecking(moleculeFileContentResponse);
     }
   }
 
   public async checkMoleculeForRules(): Promise<void> {}
+
+  private async _handleMoleculeChecking(molecule: string): Promise<string[]> {
+    try {
+      const ketMolecule =
+        await this._indigoProvider.convertToKetFormat(molecule);
+      const response =
+        await this._moleculeChecker.checkKetMolecule(ketMolecule);
+
+      return response as string[];
+    } catch (error) {
+      // throw a proper exception
+      console.error(error);
+      throw error;
+    }
+  }
 }
