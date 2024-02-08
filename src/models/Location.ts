67a96e1496508937837ce103c8a2ee107bc08443
Azat Davliatshin <azat_davliatshin@epam.com>
2024-02-09 00:03:56 +0300

    feat(EPMLSTRCHC-5) Change formats for bond string representation

--- src/models/Location.ts
+++ src/models/Location.ts
@@ -1,15 +1,9 @@
-import { type IPresentable } from "../infrastructure/types";
-
-export class Location implements IPresentable {
+export class Location {
   constructor(
     private readonly _x: number,
     private readonly _y: number,
     private readonly _z: number,
   ) {}
-
-  public toJSON(): Record<string, unknown> {
-    return { x: this._x, y: this._y, z: this._z };
-  }
 
   public get x(): number {
     return this._x;
@@ -21,5 +15,9 @@ export class Location implements IPresentable {
 
   public get z(): number {
     return this._z;
+  }
+
+  public toString(): string {
+    return `[${this._x},${this._y},${this.z}]`;
   }
 }
